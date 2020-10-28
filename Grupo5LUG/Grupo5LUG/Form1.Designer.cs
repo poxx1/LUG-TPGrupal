6e0e5192baa0ffa9c0d5fb6e4f1f1843d5e17435
poxx1 <julianlastrama@outlook.com>
2020-10-28 03:21:46 -0300

    DataSet to LinQ added

--- Grupo5LUG/Grupo5LUG/Form1.Designer.cs
+++ Grupo5LUG/Grupo5LUG/Form1.Designer.cs
@@ -28,13 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 159);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Traer info de la bd";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(8, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.Text = "dataGridView1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(125, 160);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 38);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "EXIT";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1028, 460);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
---
+++ Grupo5LUG/Grupo5LUG/LinqToDataset.cs
@@ -0,0 +1,43 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace Grupo5LUG
+{
+    class LinqToDataset
+    {
+        public void consulta()
+        {
+            //1. Genero lo necesario para realizar la consulta
+            //El connection string y la query a realizar.
+            string connectString = @"Server=AR-NB-415\SQLEXPRESS; Initial Catalog=ElasticSearch;Integrated Security=True";
+            string qy = "SELECT * From Servers";
+
+            //2. Instancion un dataAdapter para traer la info de la bd.
+            SqlDataAdapter da = new SqlDataAdapter(qy, connectString);
+
+            //Mapeo la tabla servidores a mi dataAdapter.
+            da.TableMappings.Add("Table", "Servers");
+
+            //Creo un nuevo dataset el cual va a contener la informacion que me traigo de la BD.
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            
+            //Creo una datatable para contener la informacion.
+            DataTable servers = ds.Tables["Servers"];
+
+            //Lleno la tabla con los campos que necesito utilizando LinQ
+            var query = from d in servers.AsEnumerable()
+                        select new
+                        {
+                            Name = d.Field<string>("Name"),
+                            Status = d.Field<string>("Status")
+                        };
+
+            foreach (var q in query)
+            {
+                //Console.WriteLine("Server Name = {0}, Status = {1}", q.Name, q.Status);
+            }
+        }
+    }
+}
